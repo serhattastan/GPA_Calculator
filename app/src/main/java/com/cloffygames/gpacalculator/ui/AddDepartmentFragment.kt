82c85f96a1e1f06e2103e79ae51421caa66f371d
serhattastan <serhattastannn@gmail.com>
2024-02-14 19:39:13 +0300

    Add Department Fragment mainframe added

--- app/src/main/java/com/cloffygames/gpacalculator/ui/AddDepartmentFragment.kt
+++ app/src/main/java/com/cloffygames/gpacalculator/ui/AddDepartmentFragment.kt
@@ -1,10 +1,16 @@
 package com.cloffygames.gpacalculator.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.navigation.fragment.findNavController
 import com.cloffygames.gpacalculator.R
 import com.cloffygames.gpacalculator.databinding.FragmentAddDepartmentBinding
 
@@ -13,6 +19,33 @@ class AddDepartmentFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         binding = FragmentAddDepartmentBinding.inflate(inflater, container, false)
+
+        // Bir ArrayAdapter oluşturur ve kaynak olarak bir string-array verir
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.semester_array,
+            R.layout.spinner_item
+        )
+
+        // Spinner'ın açılır menüsü için bir düzen belirler
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        // Spinner'a adapter'ı uygular
+        binding.spinner.adapter = adapter
+
+        // Spinner'dan seçilen değeri tutmak için bir değişken tanımlar
+        var selectedSemester = "0"
+        // Spinner'a bir seçim dinleyicisi atar
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Adapter'dan seçilen değeri alır ve değişkene atar
+                selectedSemester = adapter.getItem(position).toString()
+            }
+
+            // Spinner'da hiçbir seçim yapılmadığında bu metod çalışır
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Hiçbir şey yapmaz
+            }
+        }
 
         return binding.root
     }
